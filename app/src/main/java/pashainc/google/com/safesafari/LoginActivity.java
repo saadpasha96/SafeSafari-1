07babd5ee6e19fdb9b4d5a14ed890840556dcb02
saadpasha96 <saadpasha96@users.noreply.github.com>
2018-03-04 14:35:32 +0500

    removed extra var declaration

--- app/src/main/java/pashainc/google/com/safesafari/LoginActivity.java
+++ app/src/main/java/pashainc/google/com/safesafari/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     //Firebase
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-    private EditText u_email11;
+
     private EditText u_email, u_pw;
     private Button login_btn;
     private ProgressBar progressBar;
